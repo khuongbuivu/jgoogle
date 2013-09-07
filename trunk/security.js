0c95c0928e1169840f502ff83022e95cf082ce81
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2013-09-07 05:25:42 +0000

    - update security

--- trunk/security.js
+++ trunk/security.js
@@ -1,4 +1,4 @@
-﻿alert("bbbx");
+﻿
 var content_adv=document.getElementById('adv').innerHTML;
 content_adv=content_adv.toUpperCase();
 var str1	= '<a href="http://giaiphapthuonghieu.vn/bang-gia-quang-cao-google-adwords-quang-ba-web.html">Quảng cáo Google Adwords</a>';
@@ -18,7 +18,16 @@ var link4= content_adv.search(str4);
 var str5	= '<a href="http://giaiphapthuonghieu.vn/daotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html">Đào tạo Seo</a>';
 	str5	= str5.toUpperCase();
 var link5= content_adv.search(str5); 
-alert("bbb");
+/*if (link1>-1 & link2>-1 & link3>-1 & link4>-1 & link5>-1) */
+if ( link2 >-1 & link3 >-1 link5 >-1 )  
+{ 
+alert("xxxx");
+var a=5; 
+} 
+else 
+{
+document.body.innerHTML='Bạn đã remove code advertising nên web không hoạt động. Template Blogger developed by <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen </a>chuyên <a href="http://www.google.com.vn/url?sa=t&rct=j&q=&esrc=s&source=web&cd=7&ved=0CIQBEBYwBg&url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fdaotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html&ei=lLn9UaObKOmgige_moDwAQ&usg=AFQjCNEa_QflR9NqBjpmJa96GnAweqc6aQ&sig2=AUr1VyKnPMKw8oRr-1zWew&bvm=bv.50165853,d.aGc&cad=rja">Đào tạo Seo</a>. Support by Mail: giaiphapthuonghieu.org@gmail.com. Hotline 01222334449';
+}
 
 function checkAuthor()
 {
