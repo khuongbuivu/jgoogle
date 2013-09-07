86c6abfe873237e865436757303ae1b1a0f1e5de
giaiphapthuonghieu.org <giaiphapthuonghieu.org@gmail.com>
2013-09-07 06:07:26 +0000

    - update security

--- security.js
+++ security.js
@@ -1,6 +1,9 @@
-﻿alert("ccc");
+﻿
+
+function checkAuthor()
+{
 var content_adv=document.getElementById('adv').innerHTML;
-	alert("ccv1");
+	alert(content_adv);
 content_adv=content_adv.toUpperCase();
 	alert("ccc1");
 var str1	= '<a href="http://giaiphapthuonghieu.vn/bang-gia-quang-cao-google-adwords-quang-ba-web.html">Quảng cáo Google Adwords</a>';
@@ -30,8 +33,7 @@ else
 document.body.innerHTML='Bạn đã remove code advertising nên web không hoạt động. Template Blogger developed by <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen </a>chuyên <a href="http://www.google.com.vn/url?sa=t&rct=j&q=&esrc=s&source=web&cd=7&ved=0CIQBEBYwBg&url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fdaotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html&ei=lLn9UaObKOmgige_moDwAQ&usg=AFQjCNEa_QflR9NqBjpmJa96GnAweqc6aQ&sig2=AUr1VyKnPMKw8oRr-1zWew&bvm=bv.50165853,d.aGc&cad=rja">Đào tạo Seo</a>. Support by Mail: giaiphapthuonghieu.org@gmail.com. Hotline 01222334449';
 }
 */
-function checkAuthor()
-{
+
 var content_linhnguyen=document.getElementById('linhnguyen').innerHTML;
 	content_linhnguyen=content_linhnguyen.toUpperCase();
 var str6='Developed by <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen</a>'; 
