30205ceecb51088d907dfbbb8d1a259d9d520931
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2013-09-07 04:14:26 +0000

    - update

--- trunk/security.js
+++ trunk/security.js
@@ -18,7 +18,7 @@ var link4= content_adv.search(str4);
 var str5	= '<a href="http://giaiphapthuonghieu.vn/daotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html">Đào tạo Seo</a>';
 	str5	= str5.toUpperCase();
 var link5= content_adv.search(str5); 
-if (link1&gt;-1 &amp; link2&gt;-1 &amp; link3&gt;-1 &amp; link4&gt;-1 &amp; link5&gt;-1) 
+if (link1>-1 &amp; link2>-1 &amp; link3>-1 &amp; link4>-1 &amp; link5>-1) 
 { var a=5; } 
 else 
 document.body.innerHTML='Bạn đã remove code advertising nên web không hoạt động. Template Blogger developed by <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen </a>chuyên <a href="http://www.google.com.vn/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=7&amp;ved=0CIQBEBYwBg&amp;url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fdaotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html&amp;ei=lLn9UaObKOmgige_moDwAQ&amp;usg=AFQjCNEa_QflR9NqBjpmJa96GnAweqc6aQ&amp;sig2=AUr1VyKnPMKw8oRr-1zWew&amp;bvm=bv.50165853,d.aGc&amp;cad=rja">Đào tạo Seo</a>. Support by Mail: giaiphapthuonghieu.org@gmail.com. Hotline 01222334449';
@@ -29,7 +29,7 @@ var content_linhnguyen=document.getElementById('linhnguyen').innerHTML;
 var str6='Developed by <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen</a>'; 
 	str6=str6.toUpperCase();
 var linhnguyen= content_linhnguyen.search(str6); 
-if (linhnguyen&gt;-1) 
+if (linhnguyen>-1) 
 { var b=5; } 
 else 
 document.body.innerHTML='Please not remove <a href="https://plus.google.com/109004002290114308083/posts">Linh Nguyen </a> chuyên <a href="http://www.google.com.vn/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=7&amp;ved=0CIQBEBYwBg&amp;url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fdaotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html&amp;ei=lLn9UaObKOmgige_moDwAQ&amp;usg=AFQjCNEa_QflR9NqBjpmJa96GnAweqc6aQ&amp;sig2=AUr1VyKnPMKw8oRr-1zWew&amp;bvm=bv.50165853,d.aGc&amp;cad=rja">Đào tạo Seo</a>. Support by Mail: giaiphapthuonghieu.org@gmail.com. Hotline 01222334449. Please install template again';
