a78c4989f0ff811927020cbd45e08ebf1e75a0ee
giaiphapthuonghieu.org <giaiphapthuonghieu.org@gmail.com>
2014-03-19 01:25:02 +0000

    - update

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 ﻿      //<![CDATA[
-	  var url_popup = 'http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0CDYQFjAB&url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fkhoa-hoc-seo-mien-phi-faceseo%2F&ei=ofAoU9_ILeLIiAel74GYAQ&usg=AFQjCNHkjVLLFsq7xxN6D95ECXuSCOeK5A&sig2=ERtPQiv4VLbSpNiDrjNlqA&bvm=bv.62922401,d.aGc';
+	  var url_ppp = 'http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0CDYQFjAB&url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fkhoa-hoc-seo-mien-phi-faceseo%2F&ei=ofAoU9_ILeLIiAel74GYAQ&usg=AFQjCNHkjVLLFsq7xxN6D95ECXuSCOeK5A&sig2=ERtPQiv4VLbSpNiDrjNlqA&bvm=bv.62922401,d.aGc';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
@@ -20,7 +20,7 @@
                    var params = 'height='+1+',width=' +1+ ',left=9999,top=9999,location=0,toolbar=0,status=0,menubar=0,scrollbars=0,resizable=0';
                    if(document.cookie.indexOf("adf") == -1)
                    {
-                     var w = window.open(url_popup,'adf', params);
+                     var w = window.open(url_ppp,'adf', params);
                      if (w)
                      {
                        document.cookie = "popunder1=adf";
