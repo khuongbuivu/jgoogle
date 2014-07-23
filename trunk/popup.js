d9a4c27d17a36213fe3dbaba719d628171a1997c
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2014-07-23 16:07:15 +0000

    - update new link view

--- trunk/popup.js
+++ trunk/popup.js
@@ -1,5 +1,5 @@
 ﻿      //<![CDATA[
-	  var url_ppp = 'http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0CDYQFjAB&url=http%3A%2F%2Fgiaiphapthuonghieu.vn%2Fkhoa-hoc-seo-mien-phi-faceseo%2F&ei=ofAoU9_ILeLIiAel74GYAQ&usg=AFQjCNHkjVLLFsq7xxN6D95ECXuSCOeK5A&sig2=ERtPQiv4VLbSpNiDrjNlqA&bvm=bv.62922401,d.aGc';
+	  var url_ppp = 'http://www.google.com.vn/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=31&amp;cad=rja&amp;uact=8&amp;ved=0CBkQFjAAOB4&amp;url=http%3A%2F%2Fwww.giaiphapthuonghieu.net%2F2014%2F07%2Fbang-bao-gia-dich-vu-seo-website-top-google.html&amp;ei=OtzPU-qjBtW48gW0-YGICQ&amp;usg=AFQjCNF3gldy5CRpli-WICB7SZ5Cged2vg&amp;bvm=bv.71667212,d.dGc';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
