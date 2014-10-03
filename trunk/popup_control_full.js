0c3e325c823182c72f224eb2978982daf5f2bc86
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2014-10-03 04:44:10 +0000

    - update link quangcaogoogleadwords

--- trunk/popup_control_full.js
+++ trunk/popup_control_full.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://giaiphapthuonghieu.vn/dich-vu-seo-website-top-google/'><img src='https://jgoogle.googlecode.com/svn/trunk/linhnguyen.jpg' width='500px'/></a><h2>MUỐN QUẢNG CÁO NHANH XEM GIÁ<strong><a href='http://www.giaiphapthuonghieu.net/2014/07/dao-tao-seo-hcm-faceseo.html' target='_blank' title='Ðào tạo Seo website'>QUẢNG CÁO GOOGLE ADWORDS</a></strong><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='http://giaiphapthuonghieu.vn/dich-vu-seo-website-top-google/'><img src='https://jgoogle.googlecode.com/svn/trunk/linhnguyen.jpg' width='500px'/></a><h2>MUỐN QUẢNG CÁO NHANH XEM GIÁ:<br/><strong><a href='http://www.giaiphapthuonghieu.net/2014/07/dao-tao-seo-hcm-faceseo.html' target='_blank' title='Ðào tạo Seo website'>QUẢNG CÁO GOOGLE ADWORDS</a></strong><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
