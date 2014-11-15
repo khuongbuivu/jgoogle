f2beadef47ef05546851e28bc93ed1d7b2e38fe4
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2014-11-15 04:34:10 +0000

    - modify css header

--- fs/header.php
+++ fs/header.php
@@ -7,9 +7,8 @@ require_once("user.php");
   <div id="blueBarHolder" class="slim">
     <div id="blueBar" class="fixed_elem">
       <div id="pageHead" class="clearfix _5bfg" role="banner">
-        <div id="headleft"> <a href="http://faceseo.vn" > <img src="images/css/logo-faceseo.png"  style="width:340px; height:72px; margin-top:2px;"/> </a> </div>
-        <div id="headintro"> 
-          
+        <div id="headleft"> <a href="http://faceseo.vn" > <img src="images/css/logo-faceseo.png"/> </a> </div>
+        
           <!--
  Login -> view, like, G+, share -> post link, đăng banner free. <a target="_blank" href="http://giaiphapthuonghieu.vn/daotaoseo-dao-tao-seo-website-thuc-hanh-du-an-seo-thuc-te.html" ><img src="http://faceseo.vn/images/advertising/bt-faceseo-offline.png"  height="40px" /></a>
 					--> 
@@ -20,7 +19,7 @@ require_once("user.php");
 					<a href="javascript:confirm83()"><img src="http://faceseo.vn/images/advertising/bt-faceseo-share-8-3.png"  height="40px" /></a>
 					--> 
           
-        </div>
+        
         <div id="headNav" class="clearfix">
           <div class="rfloat">
             <ul id="pageNav" class="clearfix _5bfw" role="navigation">
