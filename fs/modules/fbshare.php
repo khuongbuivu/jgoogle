5bd6050ba14e69bf18ed5700a40d759e41827509
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2014-12-23 09:17:35 +0000

    - add point for user share

--- fs/modules/fbshare.php
+++ fs/modules/fbshare.php
@@ -10,14 +10,19 @@ function addpointshare()
 	global $user;
 	global $pass;
 	global $db;
-	$idUser			=	$_SESSION['session-user'];
+	// $idUser			=	'100001707050712';
+	$idUser			=	$_SESSION['session-user'];	
 	$pointBonus			=	20;
+	$dayShare			= 	7;
+	// $link			= 	'http://giaiphapthuonghieu.vn/daotaoseo.html';
 	$link			= 	$_POST['link'];
 	$con=mysqli_connect($host,$user,$pass,$db);
-	if ($_POST['point']>550 && $_SESSION['session-user']>0 || $_POST['idUser']!=$_SESSION['session-user'])
+	
+	if (($_POST['point']>550 && $_SESSION['session-user']>0) || ($_POST['idUser']!=$_SESSION['session-user']))
 	{
 			mysqli_query($con,"insert into fs_log_del (log_del_user,log_del_user_removed,log_del_id_post,log_del_type) values ('".$_SESSION['session-user']."','0','0',1)");
-	}		
+	}
+	
 	$okshare=mysqli_query($con,"select * from fs_fbshare where fbshare_iduser like '%".$idUser."%' and fbshare_link='".$link."'");
 	$result=mysqli_query($con,"select * from atw_point where idUser=".$idUser." limit 1");
 	$rsPointA = mysqli_fetch_array($result);
@@ -28,50 +33,88 @@ function addpointshare()
 	if($okshare->num_rows>0)
 	{
 		$row = mysqli_fetch_array($okshare);
-		// find index user
 		$users	= $row['fbshare_iduser'];
 		$IDU	= find($idUser,$users);
 		$times	= $row['fbshare_time'];
-		$time	= getTimeSave($times,$IDU)
+		$time	= getTimeSave($times,$IDU);
 		$timeSaved=strtotime($time);
 		$newtimes= insertStrTime($times,$IDU,$datetime);
-		$t =$timeCurrent - $timeSaved;		
-		if ($t>7*86400 || ($pointA > -1 && $pointA < 100))
+		$t =$timeCurrent - $timeSaved;	
+		if ($t>$dayShare*86400 || ($pointA > -1 && $pointA < 100))
 		{
-			mysqli_query($con,"UPDATE fs_fbshare set fbshare_time = '".$newtimes."'  where fbshare_iduser like '%".$idUser."%' and fbshare_link='".$link."'");		
+			mysqli_query($con,"UPDATE fs_fbshare set fbshare_time = '".$newtimes."'  where fbshare_iduser like '%".$idUser."%' and fbshare_link='".$link."'");
 			/*$pointrandom=rand (100,200);*/
 			$point = $pointBonus + $pointA;
-			if ($result->num_rows>0)
-				mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUser); 
-			else
-				mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUser.",".$point.")");
+			$resultB=mysqli_query($con,"select iduser from awt_list_url where url like '%".$link."%' limit 1" );		
+			if($resultB->num_rows>0)
+			{
+				if ($result->num_rows>0)
+					mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUser); 
+				else
+					mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUser.",".$point.")");
+				$row = mysqli_fetch_array($resultB)		;
+				$idUserB = $row[0];
+				$resultB=mysqli_query($con,"select * from atw_point where idUser=".$idUserB);
+				$point = 50;
+				if ($resultB->num_rows>0)
+				{
+					$row = mysqli_fetch_array($resultB);
+					$point = -$pointBonus + $row['point'];
+					$resultB=mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUserB);	
+				}
+				else
+					$resultB=mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUserB.",".$point.")");
+			}
+				
 		}
 		else
 		{
 			echo "Mỗi tuần chỉ được share 1link/1lần";
-		}	
-		
+		}			
 	}
 	else
 	{
-		// check nếu tốn tại link getlink trong database
 		$qLinks= mysqli_query($con,"select * from fs_fbshare where fbshare_link='".$link."'");
 		$rowLink=mysqli_fetch_array($qLinks);
 		if ($qLinks->num_rows>0)
 		{
-			//update list iduser
-			$newIDUs=$rowLink['fbshare_iduser'].",".$idUser;
-			mysqli_query($con,"UPDATE fs_fbshare set fbshare_iduser = '".$newIDUs."'  where fbshare_link='".$link."'");		
+			if($rowLink['fbshare_iduser']!="")
+				$newIDUs=$rowLink['fbshare_iduser'].",".$idUser;
+			else
+				$newIDUs=$idUser;
+			if($rowLink['fbshare_time']!="")
+				$newtimes	= $rowLink['fbshare_time'].",".$datetime;
+			else
+				$newtimes	= $datetime;
+			mysqli_query($con,"UPDATE fs_fbshare set fbshare_iduser = '".$newIDUs."',fbshare_time ='".$newtimes."'  where fbshare_link='".$link."'");		
 		}	
 		else
+		{
 			mysqli_query($con,"insert into fs_fbshare (fbshare_link,fbshare_iduser,fbshare_time) values ('".$link."',".$idUser.",'".$datetime."')");		
+		}
 		$result=mysqli_query($con,"select * from atw_point where idUser=".$idUser." limit 1");
 		$rsPointA = mysqli_fetch_array($result);
 		$point = $point + $rsPointA['point'];
-		if ($result->num_rows>0)
-			mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUser); 
-		else
-			mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUser.",".$point.")");
+		$resultB=mysqli_query($con,"select iduser from awt_list_url where url like '%".$link."%' limit 1" );		
+		if($resultB->num_rows>0)
+		{
+			if ($result->num_rows>0)
+				mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUser); 
+			else
+				mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUser.",".$point.")");
+			$row = mysqli_fetch_array($resultB)		;
+			$idUserB = $row[0];
+			$resultB=mysqli_query($con,"select * from atw_point where idUser=".$idUserB);
+			$point = 50;
+			if ($resultB->num_rows>0)
+			{
+				$row = mysqli_fetch_array($resultB);
+				$point = -$pointBonus + $row['point'];
+				$resultB=mysqli_query($con,"UPDATE atw_point set point = ".$point." where idUser=".$idUserB);	
+			}
+			else
+				$resultB=mysqli_query($con,"insert into atw_point (idUser,point) values (".$idUserB.",".$point.")");
+		}
 	}	
 	mysqli_close($con);	
 }
@@ -93,15 +136,17 @@ function getTimeSave($strtime,$index)
 }
 function insertStrTime($strTime,$i,$currentTime)
 {
-	return substr_replace($strTime, "$currentTime,", $i, 20);
+	return substr_replace($strTime, "$currentTime", $i*20, 19);
 }
-
 addpointshare()	;
+
 /*
 check user shared link
 Mỗi ngày chỉ được share link 1 lần vì vậy phải check link đã share
 Nếu chưa add user thì add user vào list những thành viên đã share
 Update string time 2014-04-01 10:14:06
+AddPoint User A
+Subpoint User B
 */
 
 ?>
