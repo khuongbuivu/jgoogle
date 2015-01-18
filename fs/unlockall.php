c86a36199be77ae73b390362a45798782f0810bf
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-01-18 15:58:16 +0000

    - unlock all

---
+++ fs/unlockall.php
@@ -0,0 +1,20 @@
+<?php
+include_once("config.php");
+include_once("user.php");
+global $host;
+global $user;
+global $pass;
+global $db;
+$con=mysqli_connect($host,$user,$pass,$db);
+mysqli_set_charset($con, "utf8");
+$result=mysqli_query($con,"SELECT user_id from atw_user");
+$f='us'.'er'.'_a'.'tv';
+while ($row=mysqli_fetch_array($result))
+{
+	$idUser=$row['user_id'];
+	
+	$at=md5($idUser.'1');
+	mysqli_query($con,"UPDATE atw_user set $f='".$at."' where user_id='".$idUser."'");		
+}
+mysqli_close($con);
+?>
