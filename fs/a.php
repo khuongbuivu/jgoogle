30779a798a4c3030fdccb9d2333659573a5e8d88
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-01-28 16:52:52 +0000

    -update

--- fs/a.php
+++ fs/a.php
@@ -1,12 +1,13 @@
 <?php
+/*
 $data = array();
 $replacdata = array();
-$data[0]="/function.php/";$replacdata[0]="jalsjldskjfkl.php";
+$data[0]="/function.php/";$datafiles[0]="function.php";$replacdata[0]="jalsjldskjfkl.php";
 // $data[0]="";$replacdata[0]="";
 // $data[0]="";$replacdata[0]="";
 
-$handle = fopen("activep.php", "r");
-$myfile = fopen("activep1.php", "w") or die("Unable to open file!");
+$handle = fopen($datafiles[0], "r");
+$myfile = fopen($_SERVER['DOCUMENT_ROOT'] ."/obfucate/new/".$replacdata[0], "wb") or die("Unable to open file!");
 if ($handle) {
 	while (($line = fgets($handle)) !== false) {		
 		$line = preg_replace($data, $replacdata, $line);	
@@ -15,9 +16,23 @@ if ($handle) {
 	fclose($handle);
 } else {
 	echo "keke";
-} 
-fclose($handle);
+}
 fclose($myfile);
+*/
+$dir = $_SERVER['DOCUMENT_ROOT'] ."/faceseo.vn";
+$dh  = opendir($dir);
+$i=-1;
+while (false !== ($filename = readdir($dh))) {
+    $files[] = $filename;
+	if(substr($filename,-3)=="php")
+	{
+		echo '$data['.$i.']="'.$filename.'" ; '.'$datafiles['.$i.']="'.$filename."\" ;  <br/>";
+		$i=$i+1;
+	}
+}
+// sort($files);
+// print_r($files);
+//rename("/tmp/tmp_file.txt", "/home/user/login/docs/my_file.txt");
 /*
 $string = 'The quick brown fox jumped over the lazy dog.';
 $patterns = array();
