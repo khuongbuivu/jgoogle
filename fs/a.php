fc0983104949337f30f6e32b1ef7d4de2682b0ca
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-01-30 01:55:36 +0000

    - security code

--- fs/a.php
+++ fs/a.php
@@ -1,48 +1,254 @@
 <?php
-/*
-$data = array();
-$replacdata = array();
-$data[0]="/function.php/";$datafiles[0]="function.php";$replacdata[0]="jalsjldskjfkl.php";
-// $data[0]="";$replacdata[0]="";
-// $data[0]="";$replacdata[0]="";
+$data[0]="/json_get_other_url_profile.php/" ; $datafiles[0]="json_get_other_url_profile.php" ; $replacdata[0]="5jcl4fwmkt87.php";
+$data[1]="/json_posts_new_desc_point.php/" ; $datafiles[1]="json_posts_new_desc_point.php" ; $replacdata[1]="8h61zgqt7l94.php";
+$data[2]="/json_list_user_viewing.php/" ; $datafiles[2]="json_list_user_viewing.php" ; $replacdata[2]="n952ihkq5n89.php";
+$data[3]="/json_get_link_profile.php/" ; $datafiles[3]="json_get_link_profile.php" ; $replacdata[3]="atvcn9kdl785.php";
+$data[4]="/json_list_link_friend.php/" ; $datafiles[4]="json_list_link_friend.php" ; $replacdata[4]="7iwsg23wcn88.php";
+$data[5]="/add_warning_out_view.php/" ; $datafiles[5]="add_warning_out_view.php" ; $replacdata[5]="w5fpftlb1313.php";
+$data[6]="/json_get_other_post.php/" ; $datafiles[6]="json_get_other_post.php" ; $replacdata[6]="dj3924mit186.php";
+$data[7]="/jquery.gplusone.1.1.js/" ; $datafiles[7]="jquery.gplusone.1.1.js" ; $replacdata[7]="uw54q6ghil75.js";
+$data[8]="/json_get_help_post.php/" ; $datafiles[8]="json_get_help_post.php" ; $replacdata[8]="z00kkr2s4l84.php";
+$data[9]="/get_warning_number.php/" ; $datafiles[9]="get_warning_number.php" ; $replacdata[9]="ygcxbsu97i63.php";
+$data[10]="/json_message_by_id.php/" ; $datafiles[10]="json_message_by_id.php" ; $replacdata[10]="tfjclj5o4v90.php";
+$data[11]="/jquery.carouFredSel.js/" ; $datafiles[11]="jquery.carouFredSel.js" ; $replacdata[11]="kpnpeakg4472.js";
+$data[12]="/json_comment_post.php/" ; $datafiles[12]="json_comment_post.php" ; $replacdata[12]="712znieihc82.php";
+$data[13]="/jquery.fbjlike.1.4.js/" ; $datafiles[13]="jquery.fbjlike.1.4.js" ; $replacdata[13]="k7az6ku7sf74.js";
+$data[14]="/add_point_bannerm.php/" ; $datafiles[14]="add_point_bannerm.php" ; $replacdata[14]="b3399ih26f8.php";
+$data[15]="/share-8-3-faceseo.php/" ; $datafiles[15]="share-8-3-faceseo.php" ; $replacdata[15]="gt60yda27f117.php";
+$data[16]="/add_point_banner.php/" ; $datafiles[16]="add_point_banner.php" ; $replacdata[16]="fb5ua97b7d7.php";
+$data[17]="/json_checkshareg.php/" ; $datafiles[17]="json_checkshareg.php" ; $replacdata[17]="n25bfdz0iu80.php";
+$data[18]="/banner_del_by_id.php/" ; $datafiles[18]="banner_del_by_id.php" ; $replacdata[18]="ia9fiyl8kx21.php";
+$data[19]="/change_text_link.php/" ; $datafiles[19]="change_text_link.php" ; $replacdata[19]="idtc65j1nv24.php";
+$data[20]="/getNumAnalytics.php/" ; $datafiles[20]="getNumAnalytics.php" ; $replacdata[20]="tsbeggzkkz60.php";
+$data[21]="/add_point_share.php/" ; $datafiles[21]="add_point_share.php" ; $replacdata[21]="qibhi3hgha11.php";
+$data[22]="/add_point_likeg.php/" ; $datafiles[22]="add_point_likeg.php" ; $replacdata[22]="r2hdxdqhpw10.php";
+$data[23]="/json_post_by_id.php/" ; $datafiles[23]="json_post_by_id.php" ; $replacdata[23]="nak9ficbz295.php";
+$data[24]="/content_comment.php/" ; $datafiles[24]="content_comment.php" ; $replacdata[24]="66uh1rn22k38.php";
+$data[25]="/fs_socialcount.php/" ; $datafiles[25]="fs_socialcount.php" ; $replacdata[25]="ckqgl2sn5056.php";
+$data[26]="/json_getshareg.php/" ; $datafiles[26]="json_getshareg.php" ; $replacdata[26]="jeewyqo0m283.php";
+$data[27]="/add_point_auto.php/" ; $datafiles[27]="add_point_auto.php" ; $replacdata[27]="rad6ww3iyt6.php";
+$data[28]="/json_posts_new.php/" ; $datafiles[28]="json_posts_new.php" ; $replacdata[28]="k6o7tjpu0b93.php";
+$data[29]="/add_point_like.php/" ; $datafiles[29]="add_point_like.php" ; $replacdata[29]="9bu6z5gmk49.php";
+$data[30]="/jquery1.9.1.min.js/" ; $datafiles[30]="jquery1.9.1.min.js" ; $replacdata[30]="c4l027gkux79.js";
+$data[31]="/jquery-scrollto.js/" ; $datafiles[31]="jquery-scrollto.js" ; $replacdata[31]="a1bi8uy2ud71.js";
+$data[32]="/share_birthday.php/" ; $datafiles[32]="share_birthday.php" ; $replacdata[32]="obvamhwsnn119.php";
+$data[33]="/update_message.php/" ; $datafiles[33]="update_message.php" ; $replacdata[33]="6n6bf6dkig135.php";
+$data[34]="/getNumMessage.php/" ; $datafiles[34]="getNumMessage.php" ; $replacdata[34]="o84z3qlkgx61.php";
+$data[35]="/share-faceseo.php/" ; $datafiles[35]="share-faceseo.php" ; $replacdata[35]="ykp5d9d29k118.php";
+$data[36]="/ip_click_link.php/" ; $datafiles[36]="ip_click_link.php" ; $replacdata[36]="le2qlr1dxp70.php";
+$data[37]="/statist_click.php/" ; $datafiles[37]="statist_click.php" ; $replacdata[37]="hm2vkrgvlv121.php";
+$data[38]="/statist_inday.php/" ; $datafiles[38]="statist_inday.php" ; $replacdata[38]="38116rtvfs122.php";
+$data[39]="/json_post_new.php/" ; $datafiles[39]="json_post_new.php" ; $replacdata[39]="81u2fcy4oj96.php";
+$data[40]="/update_notify.php/" ; $datafiles[40]="update_notify.php" ; $replacdata[40]="y5e2ngk5ob136.php";
+$data[41]="/json_numview.php/" ; $datafiles[41]="json_numview.php" ; $replacdata[41]="19lpt0t3yp92.php";
+$data[42]="/jquery.tipTip.js/" ; $datafiles[42]="jquery.tipTip.js" ; $replacdata[42]="9a237bw0dy77.js";
+$data[43]="/jquery.cookie.js/" ; $datafiles[43]="jquery.cookie.js" ; $replacdata[43]="2q0243qh7n73.js";
+$data[44]="/check_click1.php/" ; $datafiles[44]="check_click1.php" ; $replacdata[44]="vyyayrz9fi27.php";
+$data[45]="/subpointpost.php/" ; $datafiles[45]="subpointpost.php" ; $replacdata[45]="p1s994dkwm125.php";
+$data[46]="/templateqery.php/" ; $datafiles[46]="templateqery.php" ; $replacdata[46]="v66nhvzrz1126.php";
+$data[47]="/confirmgplus.php/" ; $datafiles[47]="confirmgplus.php" ; $replacdata[47]="8ltl02tqsc34.php";
+$data[48]="/json_my_link.php/" ; $datafiles[48]="json_my_link.php" ; $replacdata[48]="x85sd4v5fw91.php";
+$data[49]="/notifyemail.php/" ; $datafiles[49]="notifyemail.php" ; $replacdata[49]="ve91xerk5n103.php";
+$data[50]="/pagesetting.php/" ; $datafiles[50]="pagesetting.php" ; $replacdata[50]="pagesetting.php";
+$data[51]="/save_clickm.php/" ; $datafiles[51]="save_clickm.php" ; $replacdata[51]="t6im8dl0vi112.php";
+$data[52]="/save_notify.php/" ; $datafiles[52]="save_notify.php" ; $replacdata[52]="f7ah85p8wz116.php";
+$data[53]="/checkNotify.php/" ; $datafiles[53]="checkNotify.php" ; $replacdata[53]="2tu46z910d25.php";
+$data[54]="/definelocal.php/" ; $datafiles[54]="definelocal.php" ; $replacdata[54]="hrb7yfq5yr40.php";
+$data[55]="/json_comment.js/" ; $datafiles[55]="json_comment.js" ; $replacdata[55]="q23ag4x2k881.js";
+$data[56]="/del_comment.php/" ; $datafiles[56]="del_comment.php" ; $replacdata[56]="bdqw4e7aa442.php";
+$data[57]="/check_click.php/" ; $datafiles[57]="check_click.php" ; $replacdata[57]="u5dd4lerzx26.php";
+$data[58]="/status_post.php/" ; $datafiles[58]="status_post.php" ; $replacdata[58]="hg37xeubns123.php";
+$data[59]="/add_setting.php/" ; $datafiles[59]="add_setting.php" ; $replacdata[59]="eonqshghwp12.php";
+$data[60]="/ad_unlockfs.php/" ; $datafiles[60]="ad_unlockfs.php" ; $replacdata[60]="3v5179rals14.php";
+$data[61]="/testsession.php/" ; $datafiles[61]="testsession.php" ; $replacdata[61]="98pkdoclfs128.php";
+$data[62]="/jquery.tipsy.js/" ; $datafiles[62]="jquery.tipsy.js" ; $replacdata[62]="fvmaa41trl76.js";
+$data[63]="/confirmlike.php/" ; $datafiles[63]="confirmlike.php" ; $replacdata[63]="mj0ifup5yy35.php";
+$data[64]="/config_tag.php/" ; $datafiles[64]="config_tag.php" ; $replacdata[64]="w2a3tchawq33.php";
+$data[65]="/del_banner.php/" ; $datafiles[65]="del_banner.php" ; $replacdata[65]="pwgdyswdqo41.php";
+$data[66]="/help_click.php/" ; $datafiles[66]="help_click.php" ; $replacdata[66]="btguz3q7vo67.php";
+$data[67]="/jquery1.9.1.js/" ; $datafiles[67]="jquery1.9.1.js" ; $replacdata[67]="i6ptlwq0d278.js";
+$data[68]="/ad_xyzabcc.php/" ; $datafiles[68]="ad_xyzabcc.php" ; $replacdata[68]="dxhavim4t715.php";
+$data[69]="/firstlogin.php/" ; $datafiles[69]="firstlogin.php" ; $replacdata[69]="jzth9razh451.php";
+$data[70]="/save_click.php/" ; $datafiles[70]="save_click.php" ; $replacdata[70]="rf6oa6d1k1111.php";
+$data[71]="/add_pointm.php/" ; $datafiles[71]="add_pointm.php" ; $replacdata[71]="ktwyc6uks95.php";
+$data[72]="/getcontent.php/" ; $datafiles[72]="getcontent.php" ; $replacdata[72]="pvj8nm5xfv59.php";
+$data[73]="/click_link.php/" ; $datafiles[73]="click_link.php" ; $replacdata[73]="jwg2mursm528.php";
+$data[74]="/uploadfile.php/" ; $datafiles[74]="uploadfile.php" ; $replacdata[74]="z9q24h1kxg137.php";
+$data[75]="/save_link.php/" ; $datafiles[75]="save_link.php" ; $replacdata[75]="ow51znba7z115.php";
+$data[76]="/addbanner.php/" ; $datafiles[76]="addbanner.php" ; $replacdata[76]="nl8ki81x8n3.php";
+$data[77]="/get_point.php/" ; $datafiles[77]="get_point.php" ; $replacdata[77]="xbh4lbf6l562.php";
+$data[78]="/ajaxupload.js/" ; $datafiles[78]="ajaxupload.js" ; $replacdata[78]="754jzp9m4l16.js";
+$data[79]="/unlockall.php/" ; $datafiles[79]="unlockall.php" ; $replacdata[79]="ddpab165xc132.php";
+$data[80]="/add_point.php/" ; $datafiles[80]="add_point.php" ; $replacdata[80]="8nqek3jl724.php";
+$data[81]="/boxsearch.php/" ; $datafiles[81]="boxsearch.php" ; $replacdata[81]="wk4rg65fot23.php";
+$data[82]="/save_like.php/" ; $datafiles[82]="save_like.php" ; $replacdata[82]="oef7aotmpm114.php";
+$data[83]="/content1.php/" ; $datafiles[83]="content1.php" ; $replacdata[83]="oantc6j8t437.php";
+$data[84]="/function.php/" ; $datafiles[84]="function.php" ; $replacdata[84]="6dxzm0g3bd57.php";
+$data[85]="/post_new.php/" ; $datafiles[85]="post_new.php" ; $replacdata[85]="vy1uvf668l108.php";
+$data[86]="/fwarning.php/" ; $datafiles[86]="fwarning.php" ; $replacdata[86]="ny0tzlw7go58.php";
+$data[87]="/subpoint.php/" ; $datafiles[87]="subpoint.php" ; $replacdata[87]="xzn71kcrfv124.php";
+$data[88]="/birthday.php/" ; $datafiles[88]="birthday.php" ; $replacdata[88]="xtfjqabmz822.php";
+$data[89]="/save_cmt.php/" ; $datafiles[89]="save_cmt.php" ; $replacdata[89]="tufwn64f0l113.php";
+$data[90]="/del_link.php/" ; $datafiles[90]="del_link.php" ; $replacdata[90]="u8blvi4ezi43.php";
+$data[91]="/unloadfs.php/" ; $datafiles[91]="unloadfs.php" ; $replacdata[91]="hvzykhtgbf131.php";
+$data[92]="/del_post.php/" ; $datafiles[92]="del_post.php" ; $replacdata[92]="8ng1lyxhod44.php";
+$data[93]="/unlockfs.php/" ; $datafiles[93]="unlockfs.php" ; $replacdata[93]="plvmrqfob6133.php";
+$data[94]="/del_user.php/" ; $datafiles[94]="del_user.php" ; $replacdata[94]="pofjgxche445.php";
+$data[95]="/fcomment.php/" ; $datafiles[95]="fcomment.php" ; $replacdata[95]="4h5bglkgi850.php";
+$data[96]="/autoview.php/" ; $datafiles[96]="autoview.php" ; $replacdata[96]="kg4xg90oc618.php";
+$data[97]="/comment.php/" ; $datafiles[97]="comment.php" ; $replacdata[97]="ptwgalzkoq30.php";
+$data[98]="/fnotify.php/" ; $datafiles[98]="fnotify.php" ; $replacdata[98]="fsex6iqh0952.php";
+$data[99]="/activep.php/" ; $datafiles[99]="activep.php" ; $replacdata[99]="2ws8r6yfxr1.php";
+$data[100]="/fblogin.php/" ; $datafiles[100]="fblogin.php" ; $replacdata[100]="aqj1f21cfy48.php";
+$data[101]="/fbshare.php/" ; $datafiles[101]="fbshare.php" ; $replacdata[101]="gcc0kckbj849.php";
+$data[102]="/profile.php/" ; $datafiles[102]="profile.php" ; $replacdata[102]="profile.php";
+$data[103]="/content.php/" ; $datafiles[103]="content.php" ; $replacdata[103]="acgrqqmljc36.php";
+$data[104]="/xyzabcc.php/" ; $datafiles[104]="xyzabcc.php" ; $replacdata[104]="fhf2i7fdg2141.php";
+$data[105]="/config.php/" ; $datafiles[105]="config.php" ; $replacdata[105]="wn3z74w1oe32.php";
+$data[106]="/fpoint.php/" ; $datafiles[106]="fpoint.php" ; $replacdata[106]="pnxl1scmxq53.php";
+$data[107]="/tinybox.js/" ; $datafiles[107]="tinybox.js" ; $replacdata[107]="z5up1ekhm2130.js";
+$data[108]="/define.php/" ; $datafiles[108]="define.php" ; $replacdata[108]="x8rpwu2qa739.php";
+$data[109]="/comment.js/" ; $datafiles[109]="comment.js" ; $replacdata[109]="mhlgoiys7q29.js";
+$data[110]="/banner.php/" ; $datafiles[110]="banner.php" ; $replacdata[110]="banner.php";
+$data[111]="/upclick.js/" ; $datafiles[111]="upclick.js" ; $replacdata[111]="8vuzl2bs5m134.js";
+$data[112]="/gshare.php/" ; $datafiles[112]="gshare.php" ; $replacdata[112]="wz9b5qjq2365.php";
+$data[113]="/header.php/" ; $datafiles[113]="header.php" ; $replacdata[113]="wrcfrg6j4g66.php";
+$data[114]="/keycode.js/" ; $datafiles[114]="keycode.js" ; $replacdata[114]="sa6ga7ohyx97.js";
+$data[115]="/warning.js/" ; $datafiles[115]="warning.js" ; $replacdata[115]="scip3n8def140.js";
+$data[116]="/notify.php/" ; $datafiles[116]="notify.php" ; $replacdata[116]="11z5dv20jr102.php";
+$data[117]="/reset.php/" ; $datafiles[117]="reset.php" ; $replacdata[117]="i1gh79gq16110.php";
+$data[118]="/index.php/" ; $datafiles[118]="index.php" ; $replacdata[118]="index.php";
+$data[119]="/intro.php/" ; $datafiles[119]="intro.php" ; $replacdata[119]="fm7ggppqdj69.php";
+$data[120]="/notify.js/" ; $datafiles[120]="notify.js" ; $replacdata[120]="1dfnp6aeo8101.js";
+$data[121]="/fsadff.js/" ; $datafiles[121]="fsadff.js" ; $replacdata[121]="ta907vverz55.js";
+$data[122]="/group.php/" ; $datafiles[122]="group.php" ; $replacdata[122]="l8vhie102w64.php";
+$data[123]="/banner.js/" ; $datafiles[123]="banner.js" ; $replacdata[123]="1aez7ae2yt19.js";
+$data[124]="/common.js/" ; $datafiles[124]="common.js" ; $replacdata[124]="6dcapq9w9z31.js";
+$data[125]="/email.php/" ; $datafiles[125]="email.php" ; $replacdata[125]="48ex2kmwsk47.php";
+$data[126]="/point.js/" ; $datafiles[126]="point.js" ; $replacdata[126]="po18d7jpgj105.js";
+$data[127]="/post.php/" ; $datafiles[127]="post.php" ; $replacdata[127]="x0n3vuajco107.php";
+$data[128]="/test.php/" ; $datafiles[128]="test.php" ; $replacdata[128]="t2jin5wmy4127.php";
+$data[129]="/user.php/" ; $datafiles[129]="user.php" ; $replacdata[129]="d5w8vn0faf138.php";
+$data[130]="/like.php/" ; $datafiles[130]="like.php" ; $replacdata[130]="dfq1zn4orz98.php";
+$data[131]="/time.php/" ; $datafiles[131]="time.php" ; $replacdata[131]="vr5xq7rxwh129.php";
+$data[132]="/api.php/" ; $datafiles[132]="api.php" ; $replacdata[132]="72d8fyyexv17.php";
+$data[133]="/dom.php/" ; $datafiles[133]="dom.php" ; $replacdata[133]="dv9l0r966r46.php";
+$data[134]="/link.js/" ; $datafiles[134]="link.js" ; $replacdata[134]="1fdr0c1vz299.js";
+$data[135]="/spg.php/" ; $datafiles[135]="spg.php" ; $replacdata[135]="kwqfkzdw6u120.php";
+$data[136]="/8-3.php/" ; $datafiles[136]="8-3.php" ; $replacdata[136]="qrc4ei1y1x-1.php";
+$data[137]="/post.js/" ; $datafiles[137]="post.js" ; $replacdata[137]="466gs99cy1106.js";
+$data[138]="/var.js/" ; $datafiles[138]="var.js" ; $replacdata[138]="p4vbyxao8x139.js";
+$data[139]="/fs.php/" ; $datafiles[139]="fs.php" ; $replacdata[139]="ax5t6muv1a54.php";
+$data[140]="/ma.php/" ; $datafiles[140]="ma.php" ; $replacdata[140]="jleo1uaq9w100.php";
+$data[141]="/ad.js/" ; $datafiles[141]="ad.js" ; $replacdata[141]="er3od36lkq2.js"; 
+/* index.php,banner.php,group.php, profile.php, pagesetting.php */
 
-$handle = fopen($datafiles[0], "r");
-$myfile = fopen($_SERVER['DOCUMENT_ROOT'] ."/obfucate/new/".$replacdata[0], "wb") or die("Unable to open file!");
-if ($handle) {
-	while (($line = fgets($handle)) !== false) {		
-		$line = preg_replace($data, $replacdata, $line);	
-		fwrite($myfile, $line);
-	}
-	fclose($handle);
-} else {
-	echo "keke";
-}
-fclose($myfile);
-*/
-$dir = $_SERVER['DOCUMENT_ROOT'] ."/faceseo.vn";
-$dh  = opendir($dir);
-$i=-1;
-while (false !== ($filename = readdir($dh))) {
-    $files[] = $filename;
-	if(substr($filename,-3)=="php")
+
+// obfucate($datafiles,$data,$replacdata,"");
+// obfucate($datafiles,$data,$replacdata,"system");
+// obfucate($datafiles,$data,$replacdata,"modules");
+// obfucate($datafiles,$data,$replacdata,"modules/upload");
+// obfucate($datafiles,$data,$replacdata,"modules/advbanner");
+// obfucate($datafiles,$data,$replacdata,"modules/uploadimagecomment");
+// obfucate($datafiles,$data,$replacdata,"cronjob");
+// obfucate($datafiles,$data,$replacdata,"libs/getcontenturl");
+// obfucate($datafiles,$data,$replacdata,"js");
+
+// GenerateList();
+// sortArrName($datafiles,$data,$replacdata);
+
+function sortArrName($datafiles,$data,$replacdata)
+{
+	for ($ii=0;$ii<count($datafiles);$ii++)
+		for ($jj=$ii+1;$jj<count($datafiles);$jj++)
+			if(strlen($datafiles[$jj])>strlen($datafiles[$ii]))
+			{
+				$tmp=$datafiles[$ii];
+				$datafiles[$ii] = $datafiles[$jj];
+				$datafiles[$jj] = $tmp;
+				
+				$tmp=$data[$ii];
+				$data[$ii] = $data[$jj];
+				$data[$jj] = $tmp;
+				
+				$tmp=$replacdata[$ii];
+				$replacdata[$ii] = $replacdata[$jj];
+				$replacdata[$jj] = $tmp;
+				
+			};
+	for ($i=0;$i<count($datafiles);$i++)
 	{
-		echo '$data['.$i.']="'.$filename.'" ; '.'$datafiles['.$i.']="'.$filename."\" ;  <br/>";
-		$i=$i+1;
+		echo '$data['.$i.']="/'.$datafiles[$i].'/" ; '.'$datafiles['.$i.']="'.$datafiles[$i]."\" ; ".'$replacdata['.$i.']="'.$replacdata[$i].'"'.";  <br/>";		
 	}
 }
-// sort($files);
-// print_r($files);
-//rename("/tmp/tmp_file.txt", "/home/user/login/docs/my_file.txt");
-/*
-$string = 'The quick brown fox jumped over the lazy dog.';
-$patterns = array();
-$patterns[0] = '/quick/';
-$patterns[1] = '/brown/';
-$patterns[2] = '/fox/';
-$replacements = array();
-$replacements[0] = 'bear';
-$replacements[1] = 'black';
-$replacements[2] = 'slow';
-echo preg_replace($patterns, $replacements, $string);
-*/
+
+
+function obfucate($datafiles,$data,$replacdata,$folder)
+{
+
+	$dir = $_SERVER['DOCUMENT_ROOT'] ."/faceseo.vn/".$folder;
+	for ($ii=0;$ii<count($datafiles);$ii++)
+	{
+		if (file_exists($dir."/".$datafiles[$ii]))
+		{
+			echo $datafiles[$ii]."<br/>";
+			$handle = fopen($dir."/".$datafiles[$ii], "r");
+			$myfile = fopen($_SERVER['DOCUMENT_ROOT'] ."/obfucate/new/".$folder."/".$replacdata[$ii], "wb") or die("Unable to open file!".$datafiles[$ii-1]);
+			if ($handle) {
+				while (($line = fgets($handle)) !== false) {		
+					$line = preg_replace($data, $replacdata, $line);	
+					fwrite($myfile, $line);
+				}
+				fclose($handle);
+			} else {
+				echo "keke";
+			}
+			fclose($myfile);
+		}
+	}
+
+}
+function GenerateList()
+{
+	$dir = $_SERVER['DOCUMENT_ROOT'] ."/obfucate/allfile";
+	$dh  = opendir($dir);
+	$i=-1;
+	while (false !== ($filename = readdir($dh))) {
+		$files[] = $filename;
+		if((substr($filename,-3)=="php" || substr($filename,-2)=="js") && (!checkInArray($datafiles,"user.php")) 
+		&& ($filename!="index.php") && ($filename!="banner.php")&& ($filename!="profile.php")&& ($filename!="pagesetting.php"))
+		{
+			if (substr($filename,-3)=="php")
+				echo '$data['.$i.']="/'.$filename.'/" ; '.'$datafiles['.$i.']="'.$filename."\" ; ".'$replacdata['.$i.']="'.generateRandomString(10).$i.'.php"'.";  <br/>";
+			else
+				echo '$data['.$i.']="/'.$filename.'/" ; '.'$datafiles['.$i.']="'.$filename."\" ; ".'$replacdata['.$i.']="'.generateRandomString(10).$i.'.js"'.";  <br/>";
+			$i=$i+1;
+		}
+		else if (($filename=="index.php") || ($filename=="banner.php")|| ($filename=="profile.php") || ($filename=="pagesetting.php"))
+		{
+				echo '$data['.$i.']="/'.$filename.'/" ; '.'$datafiles['.$i.']="'.$filename."\" ; ".'$replacdata['.$i.']="'.$filename.'"'.";  <br/>";
+				$i=$i+1;
+		}
+	}
+}
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+function checkInArray($arr,$key)
+{
+	for ($i=0;$i<count($arr);$i++)
+	{
+		if($arr[$i]==$key)
+			return true;
+	}
+	return false;
+}
+
 ?>
