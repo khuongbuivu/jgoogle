c5a513bac408861b851e504843969c30d1f2b83d
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-02-01 01:48:20 +0000

    - update

--- fs/modules/cronjob/post.php
+++ fs/modules/cronjob/post.php
@@ -8,9 +8,8 @@
 	$con=mysqli_connect($host,$user,$pass,$db);
 	$resultpost=mysqli_query($con,"Select post_id FROM atw_post");
 	echo $resultpost->num_rows;
-	$numdel=$resultpost->num_rows - 10;	
+	$numdel=$resultpost->num_rows - 300;	
 	$listIds=mysqli_query($con,"SELECT post_id FROM atw_post  where post_id>1 order by post_id desc limit 10,".$numdel);
-	mysqli_query($con,"DELETE FROM atw_notify  where notify_id_post>1");
 	while ($rowId=mysqli_fetch_array($listIds))
 	{
 		mysqli_query($con,"DELETE FROM atw_post  where post_id=".$rowId['post_id']);
