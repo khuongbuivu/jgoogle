1086ba58850d9f889ed6b64521550e684c2a58bd
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-03-04 11:16:39 +0000

    - UPDATE

--- fs/index.php
+++ fs/index.php
@@ -829,10 +829,12 @@ $('body').on('keyup','textarea,.contentbox', function(e) {
 		var name = $("#name").html();			
 		var token = generateToken();
 		var listTags= $(".mentionsHidden"+idArt).val();
-		addLinkToDb(url1,<?php echo $id_user; ?>,tb.text());
-		addCmtToDb(url,idArt,tb.text(), $("#imgSrc"+idArt).html(), imgLogo, name, idUser, token);		
-        subPoint(idUser,sidUser,-5,tb.text());
-		addNotify(url_notify,idUser,name,imgLogo,idArt,0,tb.text() + $("#imgSrc"+idArt).html(),0,listTags);
+		var textCmt=tb.text();
+		textCmt=textCmt.replace("###"," ###");
+		addLinkToDb(url1,<?php echo $id_user; ?>,textCmt);
+		addCmtToDb(url,idArt,textCmt, $("#imgSrc"+idArt).html(), imgLogo, name, idUser, token);		
+        subPoint(idUser,sidUser,-5,textCmt);
+		addNotify(url_notify,idUser,name,imgLogo,idArt,0,textCmt + $("#imgSrc"+idArt).html(),0,listTags);
 		$("#imgSrc"+idArt).html("");
 		$(this).html("");	
 		$(this).css("height","20px");
@@ -1107,7 +1109,6 @@ function confirmlink( link ) {
 	}
 };
 
-//eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('4 8(0,9){7 5=b.f("i://g.k/8.3?0="+0,"j","e=6, a=6, c=d, h=1, z=1, v=u, l=1");5.y=4(){t(s+"n.3",m);7 2=o.p(\'r\'+9);2.q.w=2.x}};',36,36,'link|500|iframe|php|function|windowLike|no|var|confirmgplus|id|scrollbars|window|resizable|yes|toolbar|open|faceseo|top|http|_blank|vn|height|idUser|get_point|document|getElementById|contentWindow|iframegplus|root_path|getPoint|550|width|location|src|onbeforeunload|left'.split('|'),0,{}))
 function confirmgplus(link,id){var windowLike=window.open(root_path + "confirmgplus.php?link="+link,"_blank","toolbar=no, scrollbars=no, resizable=yes, top=500, left=500, width=550, height=500");windowLike.onbeforeunload=function(){getPoint(root_path+"get_point.php",idUser);var iframe=document.getElementById('iframegplus'+id);iframe.contentWindow.location=iframe.src}};
 function fsGShare(url,token) {
 	var resultstart=0;
