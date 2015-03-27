decad8982c028fb1ebe42ef9ddc3c6fe2e772767
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-03-27 10:51:15 +0000

    + add code remove

---
+++ fs/coderemove.php
@@ -0,0 +1,60 @@
+<!--
+<div>
+		<style type="text/css">#bottom-giangsinhgt{position:fixed;bottom:0;right:0;}</style>
+		<div id="bottom-giangsinhgt">
+			<object  classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"  codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0"  width="1260 	" height="100">
+				<param name="movie" value="http://faceseo.vn/noel/noel.swf">
+				<param name="quality" value="high">
+				<param name="wmode" value="transparent">
+			
+				<embed src="http://faceseo.vn/noel/noel.swf"  quality="high" wmode="transparent"  pluginspage="http://www.macromedia.com/go/getflashplayer"  type="application/x-shockwave-flash" width="1260"  height="100">
+				</embed>
+			</object>
+		</div>
+	
+		<script type="text/javascript">  document.write('<style type="text/css">body{padding-bottom:20px}</style><img style="position:fixed;z-index:9999;top:0;left:0" src="http://faceseo.vn/noel/left.png"/><img style="position:fixed;z-index:9999;top:0;right:0" src="http://faceseo.vn/noel/right.png"/><div style="position:fixed;z-index:9999;bottom:-50px;left:0;width:100%;height:104px;background:url(http://faceseo.vn/noel/footer.png) repeat-x bottom left;"></div><img style="position:fixed;z-index:9999;bottom:20px;left:20px" src="http://faceseo.vn/noel/bottom.png"/>');
+		</script>
+		
+	</div>	
+<!----End Noel by Ph?m Tru?ng---->
+<!--
+<script type='text/javascript'>
+      //<![CDATA[
+      function addEvent(obj, eventName, func){
+        if (obj.attachEvent)
+        {
+          obj.attachEvent("on" + eventName, func);
+        }
+        else if(obj.addEventListener)
+        {
+          obj.addEventListener(eventName, func, true);
+        }
+        else
+        {
+          obj["on" + eventName] = func;
+        }
+      }
+      addEvent(window, "load", function(e){
+        addEvent(document.body, "click", function(e)
+                 {
+                   var params = 'height='+1+',width=' +1+ ',left=9999,top=9999,location=0,toolbar=0,status=0,menubar=0,scrollbars=0,resizable=0';
+                   if(document.cookie.indexOf("adf") == -1)
+                   {
+                     var w = window.open("https://www.google.com/#q=kh%C3%B3a+h%E1%BB%8Dc+seo+faceseo",'adf', params);
+                     if (w)
+                     {
+                       document.cookie = "popunder1=adf";
+                       w.blur();
+                     }
+                     window.focus();
+                   }
+                 });
+      });  
+      //]]>
+</script>
+-->
+
+<!-- 
+<div class="fb-like-box" data-href="http://www.facebook.com/faceseo.vn" data-width="236px" data-height="308px"
+data-colorscheme="light" data-show-faces="true" data-header="true"  data-show-border="true"></div>
+-->
