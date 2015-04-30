c283a77c768cc64201dd868533230f2dbe7dfb4e
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-04-30 04:48:11 +0000

    + add menu to file

--- fs/index.php
+++ fs/index.php
@@ -303,28 +303,9 @@ exit();
 		<div>
 		<div id="mainContainer">
 			<div id="leftCol" class="leftCol">
-              <ul class="icon-setting">
-				 <li><span class="icon-set icon-home" onclick="return openLinkMenu('<?php echo $PATH_ROOT; ?>');"></span></li>
-				<li>
-				<span class="icon-set icon-view" id="fsAnaylyticsButton"></span><a class="jewelButton a-view"><span class="jewelCount" id="anaylyticsCountWrapper" ><span id="anaylyticsCountValue">99</span></span> </a>
-				</li>				
-				<li>
-				<span class="icon-set icon-loa" id="fbNotificationsJewel"></span><a class="jewelButton icon-comment"><span class="jewelCount" id="notificationsCountWrapper" ><span id="notificationsCountValue">1</span></span> </a>				
-				</li>				
-				<li>
-				<span class="icon-set icon-message" id="iconemailbutton"></span><a class="jewelButton icon-email"> <span class="jewelCount" id="emailCountWrapper" ><span id="iconemailCountValue">1</span></span> </a>
-				</li>
-				<li><span class="icon-set icon-gplus" onclick="return openLinkMenu('<?php echo $PATH_ROOT."group.php?idgroup=1"; ?>');"></span> </li>
-				<li id="icon-firefox"><span class="icon-set icon-firefox" onclick="return openLinkMenu('http://giaiphapthuonghieu.vn/faceseo1.4.xpi');" title="Cài đặt Addon FS1.4 để clickkeywords được +50Đ"></span></li>
-<li><span class="icon-set icon-upbanner" onclick="return openLinkMenu('<?php echo "http://".DOMAIN."/modules/upload/banner.php"; ?>');"></span> </li>
-				<li><span class="icon-set icon-setting" onclick="return openLinkMenu('<?php echo $PATH_ROOT."pagesetting.php"; ?>'); " href="<?php echo $PATH_ROOT."pagesetting.php"; ?>"></span></li>				
-
-				
-				<!-- <li><span class="icon-set icon-connect" onclick="return openLinkMenu('<?php //echo $PATH_ROOT."l8vhie102w64.php?idgroup=2"; ?>');"></span> </li>				
-				<li><a href=""><span class="icon-set icon-share"></span> </a></li>
-				-->
-				
-               </ul>
+				<?php
+				require_once("menu.php");
+				?>
 			  <script>
 				var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 				if (is_firefox!==true)
