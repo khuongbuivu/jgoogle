ad9c0ebcd67a5389b899e13c6fcb903b4fb0cf37
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-06-21 17:45:55 +0000

    - update new design

--- fs/unlockfs.php
+++ fs/unlockfs.php
@@ -18,64 +18,13 @@ require_once("definelocal.php");
 </head>
 <body class="hasSmurfbar">
 <div id="container">
-
- <?php
-	include_once("header.php");
- ?>
  
-  <div id="content">
-    <div id="showindex" class="ccenter2">
-       <div class="textadmin">Contact Admin<br />
-Unlock your account</div>
-        <ul class="bankli">
-        <li><div class="bankcontent">
-        <div class="bankimg"><span class="vietcombank"></span></div>
-        <div class="banktext">0251001616907<br />
-Nguyen Hong Linh</div>
-        </div></li>
-        <li><div class="iconmoney"></div></li>
-        <li><div class="bankcontent">
-        <div class="bankimg bidv"></div>
-        <div class="banktext">58110000196649<br />Nguyen Hong Linh</div>
-        </div></li>     
-        </ul>
-
-
-    </div>
-    <div id="gioithieu" class="ccenter c1" style="display:none">
-     
-    </div>
-    <div id="dieukhoan" class="ccenter c2" style="display:none">
-      <h1>Điều khoản</h1>
-      <div class="noidung"></div>
-    </div>
-    <div id="baomat" class="ccenter c3" style="display:none">
-      <h1>Bảo mật</h1>
-      <div class="noidung"></div>
-    </div>
-    <div id="hoptac" class="ccenter c4" style="display:none">
-      <h1>Hợp tác</h1>
-      <div class="noidung"></div>
-    </div>
-    <div id="tuyendung" class="ccenter c5" style="display:none">
-      <h1>Tuyển dụng</h1>
-      <div class="noidung"></div>
-    </div>
-  </div>
-  <div id="footerOther">  
-	<!--
-	<div class="footermenu">
-      <ul>
-        <li><span data-show="1" class="liamenu">Giới thiệu</span><span class="subicon">New</span></li>
-        <li><span data-show="2" class="liamenu">Điều khoản</span></li>
-        <li><span data-show="3" class="liamenu">Bảo mật</span></li>
-        <li><span data-show="4" class="liamenu">Hợp tác</span></li>
-        <li><span data-show="5" class="liamenu">Tuyển dụng</span></li>
-      </ul>
+	<div id="content">
+		<div id="showindex" class="ccenter2">
+		   <a href="https://www.facebook.com/nguyenlinh.ceo.faceseo"><img src="images/active.png"></a>
+		</div>
 	</div>
-	-->
-	Copyright 2013 Faceseo
-  </div>
+	
 </div>
 <script src="js/enscroll-0.6.0.min.js"></script> 
 <script>
