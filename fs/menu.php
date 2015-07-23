5bfc3503ee76b30af23495f5405102b4a056cd74
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-07-23 10:40:32 +0000

    + add rule post banner

--- fs/menu.php
+++ fs/menu.php
@@ -1,7 +1,18 @@
  <?php
- include_once("definelocal.php");
+	include_once("definelocal.php");
+	include_once("config.php");
+	global $host;
+	global $user;
+	global $pass;
+	global $db;
+	$idUser	=	$_SESSION['session-user'];
+	$con=mysqli_connect($host,$user,$pass,$db);
+	$result=mysqli_query($con,"select * from atw_point where idUser=".$idUser);
+	while ($row = mysqli_fetch_array($result))
+	{
+		$point = $row['point'];
+	}
  ?>
- 
  <ul class="icon-setting">
 				 <li><span class="icon-set icon-home" onclick="return openLinkMenu('<?php echo $PATH_ROOT; ?>');"></span></li>
 				<li>
@@ -15,6 +26,10 @@
 				</li>
 				<li><span class="icon-set icon-gplus" onclick="return openLinkMenu('<?php echo $PATH_ROOT."index.php?idgroup=1"; ?>');"></span> </li>
 				<li id="icon-firefox"><span class="icon-set icon-firefox" onclick="return openLinkMenu('http://giaiphapthuonghieu.vn/faceseo1.4.xpi');" title="Cài đặt Addon FS1.4 để clickkeywords được +50Đ"></span></li>
+				<?php if ($point>$MINPOSTBANNER): ?>
 				<li><span class="icon-set icon-upbanner" onclick="return openLinkMenu('<?php if(LOCAL==true ) echo "http://localhost/".DOMAIN."/modules/upload/banner.php"; else echo "http://".DOMAIN."/modules/upload/banner.php"; ?>');"></span> </li>
+				<?php else:?>
+				<li><span class="icon-set icon-upbanner" onclick="alert('Bạn được đăng banner khi tài khoản được 10.000 điểm.');"></span> </li>
+				<?php endif ?>
 				<li><span class="icon-set icon-setting" onclick="return openLinkMenu('<?php echo $PATH_ROOT."pagesetting.php"; ?>'); " href="<?php echo $PATH_ROOT."pagesetting.php"; ?>"></span></li>
 </ul>
