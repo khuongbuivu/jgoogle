e067cc6ef847f0c7e7ef09ab52db9ea91ab3af0f
giaiphapthuonghieu.org@gmail.com <giaiphapthuonghieu.org@gmail.com@ccc5a29c-c454-f20a-839f-464db8a12d37>
2015-08-09 08:44:59 +0000

    - update

--- fs/seo/faceseo-la-gi/menutop.php
+++ fs/seo/faceseo-la-gi/menutop.php
@@ -3,14 +3,12 @@ include_once("../../hrb7yfq5yr40.php");
 include_once("../../x8rpwu2qa739.php");
 ?> 
 <ul class="fl menu-main">
-	<li><a href="<?php echo FULLDOMAIN; ?>">Trang chủ</a></li>
-	<li class="current"><a href="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/"; ?>" class="">Giới thiệu</a></li>
-	<li><a href="#">Dịch vụ Seo</a></li>
-	<li><a href="#">Trợ giúp</a></li>
+	<li><a href="<?php echo FULLDOMAIN; ?>">Trang chủ</a></li>
+	<li class="current"><a href="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/"; ?>" class="">Giới thiệu</a></li>
+	<li><a href="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/huong-dan-su-dung-faceseo.php"; ?>">Hướng dẫn</a></li>		<li><a href="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/dich-vu-faceseo.php"; ?>">Dịch vụ Seo</a></li>	
 </ul>
 <select class="m-menutop" onchange="window.location=this.value">
-<option value="<?php echo FULLDOMAIN; ?>">Trang chủ</option>
-<option value="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/"; ?>">Giới thiệu</option>
-<option value="#">Dịch vụ Seo</option>
-<option value="#">Trợ giúp</option>
+<option value="<?php echo FULLDOMAIN; ?>">Trang chủ</option>
+<option value="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/"; ?>">Giới thiệu</option><option value="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/huong-dan-su-dung-faceseo.php"; ?>">Hướng dẫn</option>
+<option value="<?php echo FULLDOMAIN."/seo/faceseo-la-gi/dich-vu-faceseo.php"; ?>">Dịch vụ Seo</option>
 </select>
